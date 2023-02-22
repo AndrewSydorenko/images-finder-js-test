0f2c9301d074b701f28b089959e1d652d3218868
AndrewSydorenko <98640251+AndrewSydorenko@users.noreply.github.com>
2023-02-22 12:55:49 +0000

    Deploying to gh-pages from @ AndrewSydorenko/goit-js-hw-10@202280866a831f2678151fb53f0c43debc40eea6 🚀

--- 01-color-switcher.a8d950aa.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)}),{once:!0}),e.addEventListener("click",(function(){clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.a8d950aa.js.map
