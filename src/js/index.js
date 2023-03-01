fd9ed3be600f37f6f16fe9e219c7ee258cb57459
Sam <98640251+AndrewSydorenko@users.noreply.github.com>
2023-03-01 14:54:29 +0200

    fix button

--- src/js/index.js
+++ src/js/index.js
@@ -2,19 +2,32 @@ import '../css/styles.css';
 import Notiflix from 'notiflix';
 import { GetImages } from "./api/GetGallery";
 import { GalleryMarkup, galleryClear } from "./markup/markupGallery";
+import { throttle } from 'lodash';
 
 const serchForm = document.querySelector('.search-form')
 const loadMoreBtn = document.querySelector('.load-more')
 
 serchForm.addEventListener('submit', serchInputHandler)
 loadMoreBtn.addEventListener('click', loadMoreHandler)
-loadMoreBtn.disabled = true;
-loadMoreBtn.style.display = "none";
+
+
+function showButton() {
+    loadMoreBtn.style.display = "flex";
+    loadMoreBtn.disabled = false;
+}
+
+function hibeButton() {
+    loadMoreBtn.style.display = "none";
+    loadMoreBtn.disabled = true;
+}
+hibeButton()
+
 let page = 1;
 let searchRequest = '';
 
 async function serchInputHandler(event) {
     event.preventDefault();
+    hibeButton()
     searchRequest = event.target.searchQuery.value.trim();
     if (!searchRequest) {
         return
@@ -29,19 +42,20 @@ async function serchInputHandler(event) {
         Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
         return
     }
-    loadMoreBtn.disabled = false;
-    loadMoreBtn.style.display = "flex";
+    showButton()
     GalleryMarkup(data)
     event.target.reset()
 }
 async function loadMoreHandler(event) {
+    hibeButton()
     page += 1;
     const data = await GetImages(searchRequest, page)
     GalleryMarkup(data)
     if (pageCheck(data.totalHits)) {
         Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
         event.target.disabled = true;
-        loadMoreBtn.style.display = "none";
+    } else {
+        showButton()
     }
 
 }
@@ -50,3 +64,4 @@ function pageCheck(totalHits) {
     const allPages = Math.ceil(totalHits / 40);
     return allPages === page;
 }
+
